d1e3265d2800e18699a70b3a08065a90c85c99d2
cesar angulo <cesarangulo@cesars-MacBook-Air-2.local>
2021-05-31 19:49:37 -0600

    se definen codigo para inputs de parametros

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from deap import creator
 from deap import tools
 from deap import algorithms
 import numpy
+import matplotlib.pyplot as plt
 toolbox = base.Toolbox()
 
 
@@ -12,8 +13,6 @@ toolbox = base.Toolbox()
 creator.create("FitnessMin",base.Fitness,weights=(-1.0,))
 creator.create("Individual",list,fitness=creator.FitnessMin)
 toolbox.register('attr_bool',random.randint,0,1)
-
-
 toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.attr_bool, 5)
 toolbox.register('population',tools.initRepeat, list, toolbox.individual)
 
@@ -22,19 +21,13 @@ toolbox.register('population',tools.initRepeat, list, toolbox.individual)
 #restriccion donde x4+x5 <= 1 o ser penalizado
 def restriccion2(individual):
     x1, x2, x3, x4, x5 = [individual[i] for i in (0,1,2,3,4)]
-    suma = x5+x4
-    if suma <= 1:
-        return True
-    else:
-        return False
+    return True if x5+x4 <= 1 else False
 
 #primer restricion donde x1+x2+x3+x4+x4 = 3 para aprobar la restriccion o ser penalizado
 def restriccion1(individual):
     suma = sum(individual)
-    if suma == 3:
-        return True
-    else:
-        return False
+    return True if suma == 3 else False
+
 #una vez que definimos todad las restricciones llamamos esta fucion que se le pasara al toolbox como parametro
 #este hara su trabajo evolucionando los cromosomas y implementando dicha funcion para calcular los valores de aptitudes
 # de los nuevos miebros de la poblacion
@@ -50,33 +43,74 @@ def aptitudIndividuo(individual):
   
     return aptitud,
 
-    
-toolbox.register('evaluate',aptitudIndividuo)
-toolbox.register("mate",tools.cxTwoPoint)
-toolbox.register("mutate",tools.mutFlipBit,indpb=0.50)
-toolbox.register("select",tools.selTournament,tournsize=2)
 
 
-def main():
-    pop = toolbox.population(n=2)
+
+def algoritmo(poblacion,cbxpb,mutpb,ngen):
+    #se define el tamaño que tendra la poblacion inicial
+    N= int(poblacion)
+    pop = toolbox.population(n=N)
     print('poblacion inicial',pop)
-    CBXPB,MUTPB,NGEN = 0.5,0.2,15
+    #se definen aqui las constants de probabilidad de cruze tambien la de mutacion
+    #CBXP {probabilidade de cruza}
+    #MUTPB {probabilidad de mutacion}
+    #NGEN {numero de generaciones despues de la poblacion inicial}
+    CBXPB = float(cbxpbbueb)
+    MUTPB = float(mutpb)
+    NGEN =  int(ngen)
     hof = tools.HallOfFame(1)
+    # en este objeto asignamois 4 arreglos de numpy
+    # estos arreglos los registramos a las funciones genericas de stadisticas para que el algoritmo las pueble
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register('avg',numpy.mean)
-    stats.register('std',numpy.std)
+    stats.register('sdt',numpy.std)
     stats.register('min',numpy.min)
     stats.register('max',numpy.max)
+    #inicializamos el algoritmo pasando nuestra poblacion la clase hall of fame y nuetras constantes del problema
+    pop,logbook = algorithms.eaSimple(pop, toolbox,cxpb=CBXPB,mutpb=MUTPB,ngen=NGEN,stats=stats,halloffame=hof)
 
-    algorithms.eaSimple(pop, toolbox,cxpb=CBXPB,mutpb=MUTPB,ngen=NGEN,stats=stats,halloffame=hof)
+    return pop, logbook, hof
 
-    return pop, stats, hof
+def main(poblacion,CBXPB,MUTPB,NGEN):
+    pops,log,hof = algoritmo(poblacion,CBXPB,MUTPB,NGEN)
+    gen, avg,min_, max_ = log.select('gen','avg','min','max')
+    plt.plot(gen,avg, label="average")
+    plt.plot(gen,min_,label="minimum")
+    plt.plot(gen,max_,label="maximum")
+    plt.xlabel("Generation")
+    plt.ylabel("Fitness")   
+    plt.legend(loc="lower right")
+    plt.show()
+    print('el individuo con mas optimo para esta funcion es', hof)
+
+#registra el tipo de cruza seleccionado
+def mateType(type,toolbox):
+    print(type)
+    if(type == "1"):
+        toolbox.register("mate",tools.cxOnePoint)
+    elif(type == "2"):
+         toolbox.register("mate",tools.cxTwoPoint)
+    elif(type == "3"):
+         toolbox.register("mate",tools.cxUniform,indpb=0.7)
+
+
 
 if __name__ == "__main__":
-    pops,stats,hof = main()
-    print('looking at stats', stats)
-    print('el mejor individuo de acuerdo a la funcion',hof)
-   
+        population = input("Ingrese el tamanio de la poblacion  :")     
+        print("por favor seleccione el tipo de cruza")  
+        cruza = input('[1] - cruza 1 punto [2] - cruza 2 puntos [3] - cruza uniforme    :') 
+        probabilidadCX = input("por favor ingrese la probabilidad de cruza [0.1-0.99] :")
+        probabilidadMX = input("por favor ingrese la probabilidad de mutacion [0.1-0.99] :")
+        ngen = input("intruduzca el numero de generaciones a generar   :")
+        mateType(cruza,toolbox)
+        toolbox.register('evaluate',aptitudIndividuo)
+        toolbox.register("mutate",tools.mutFlipBit,indpb=0.50)
+        toolbox.register("select",tools.selTournament,tournsize=2)
+        main(population,probabilidadCX,probabilidadMX,ngen)
+    
+
+ 
+
 
 
 
